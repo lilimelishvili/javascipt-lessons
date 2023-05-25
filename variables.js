6933843b145ce1ca313c947a83a2806fdd76362b
lilimelishvili <91181560+lilimelishvili@users.noreply.github.com>
2023-05-25 10:35:15 +0400

    finish Variable

--- variables.js
+++ variables.js
@@ -1,1 +1,18 @@
-// test
+
+let num = 2 + 2
+console.log( num )
+ 
+num = 9 
+console.log (num)
+
+let userName  = "Lily"
+console.log( username )
+
+//"let" it  will say okay 
+//"num" is variable name 
+// "equal" is inbuilt what is the number right so of course we build 
+//"log" is in build 
+
+
+
+
