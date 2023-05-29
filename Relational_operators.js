78070975e6d97c8112ebf15acd113431025b998d
lilimelishvili <91181560+lilimelishvili@users.noreply.github.com>
2023-05-29 21:31:26 +0400

    Relational_operators-done

---
+++ Relational_operators.js
@@ -0,0 +1,14 @@
+
+//let data = 7 > 4 
+
+//console.log( data);
+
+//let x = 3 
+//let y = 3 
+// let data = x > y 
+//console.log(data)
+let x = ''
+let y = false
+let data  = x === y 
+console.log (data)
+
