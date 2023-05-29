f22cf1a0595db1368cc074dfecdfcc9f62ffca7b
lilimelishvili <91181560+lilimelishvili@users.noreply.github.com>
2023-05-29 20:52:59 +0400

    Arithmetic_Operators_Done

---
+++ Arithmetic_Operators.js
@@ -0,0 +1,53 @@
+//let num1 = 5
+//let num2  = 4
+//let result = num1 + num2
+//console.log(result)
+
+//let num3 = 6 
+//let num4 = 3 
+//let result = num3 - num4
+//console.log(result)
+
+//let num3 = 6 
+//let num4 = 3 
+//let result = num3 * num4
+//console.log(result)
+
+//let num3 = 6 
+//let num4 = 3 
+//let result = num3 / num4
+//console.log(result)
+
+//let num3 = 6 
+//let num4 = 3 
+//let result = num3 % num4
+//console.log(result)
+
+//let num3 = 7
+//let num4 = 3 
+//let result = num3 % num4
+//console.log(result)
+
+//let num3 = true
+//let num4 = true
+//let result = num3 + num4
+//console.log(result)
+
+let num = 4 
+num ++     // C++ is an increment  & C-- is a decrement 
+
+// ++num is a pre increment   // --num is a pre decrement
+//num ++ is post increment   //num -- is post decrement 
+
+console.log(num);
+
+let num1 = 4
+let result = 4 ** 3 
+//let result = Math.pow(4,3) //(num1*num1*num1*)
+console.log(result) 
+
+
+
+
+
+
