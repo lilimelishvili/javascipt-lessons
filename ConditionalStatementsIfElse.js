38bef43c2dc2d919fa77f60ad27fdb23d4006fa1
lilimelishvili <91181560+lilimelishvili@users.noreply.github.com>
2023-06-05 22:47:35 +0400

    IfElse

---
+++ ConditionalStatementsIfElse.js
@@ -0,0 +1,18 @@
+// IF else
+
+let num1 = 10;
+let num2 = 13;
+let num3 = 7 
+ 
+
+if (num1 > num2 &&  num1 > num3 ){
+  console.log("num1 is greater");
+}
+else  if (num2 > num1 && num2 > num3){
+  console.log("num2 is greater")
+  
+}
+else {
+    console.log("num3 is greater")
+}
+console.log ("bye....")
