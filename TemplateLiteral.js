22fed4238f73901cefafa8d54417be825091a694
lilimelishvili <91181560+lilimelishvili@users.noreply.github.com>
2023-06-06 18:58:01 +0400

    TemplateLiteral

---
+++ TemplateLiteral.js
@@ -0,0 +1,6 @@
+let num1 = 8 
+let num2 = 7 
+let result = num1 + num2 
+ console.log(`The addition of ${num1} + ${num2} is ${result}`)
+ console.log('My name is \n Lily')
+// console.log (" The addition of " + num1 + " and " + num2 + " is " + result )
