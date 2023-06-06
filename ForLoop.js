9812b3fd2a9f64f26e4560d03c4ce317371d37e7
lilimelishvili <91181560+lilimelishvili@users.noreply.github.com>
2023-06-06 19:50:11 +0400

    While vs For Loop

--- ForLoop.js
+++ ForLoop.js
@@ -1,6 +1,14 @@
 
-for(let i=1;i<=5;i++){
-    console.log('Hi', i);
-    for(let j=1;j<=5;j++)
-    console.log('Hello', j);
+//for(let i=1;i<=100;i++){
+    //if(i%3===0)
+    //console.log(i);
+//}
+
+let num = 565783 
+let num2 = 0 
+while(num>0){
+ console.log(num%10)
+ num = parseInt(num / 10 )
 }
+console.log(num2);
+
