737d39c6bcb235f2434df410430591bb05ffe22e
lilimelishvili <91181560+lilimelishvili@users.noreply.github.com>
2023-06-10 08:55:16 +0400

    functionExpression

--- Functions.js
+++ Functions.js
@@ -11,9 +11,23 @@ console.log(str);
 /* -------------------------------------------------------------------------- */
 
 function greet2(a, b) {
-    return `price = ${a * b} ლარი.`;
+   return `price = ${a * b} ლარი.`;
 }
 
   
-  console.log(greet2(5, 10));
+ console.log(greet2(5, 10));
+/* -------------------------------------------------------------------------- */
+
+
+let add = function(num1, num2)
+ {
+    return num1 + num2 ;
+ }
+ let sum = add
+ let result = sum(5,7)
+
+//let result  =  add(5,7)
+console.log(result);
+
+
 
