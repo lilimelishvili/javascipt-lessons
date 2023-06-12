e672bdb3dc308662d339194863c092c619b396c6
lilimelishvili <91181560+lilimelishvili@users.noreply.github.com>
2023-06-12 18:37:14 +0400

    arrowFunction

---
+++ ArrowFunction.js
@@ -0,0 +1,9 @@
+//let greet = (User) => {
+ //   return 1 ;
+//}
+//console.log(greet('Lily'));
+
+let add = (num1,num2) => num1 + num2;
+                   
+let result = add (5,6)
+console.log(result);
