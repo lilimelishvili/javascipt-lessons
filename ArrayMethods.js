03ea70a5e2778d09f3071f66a74cf894846eb0ea
lilimelishvili <91181560+lilimelishvili@users.noreply.github.com>
2023-06-26 18:31:40 +0400

    ArrayMethods

---
+++ ArrayMethods.js
@@ -0,0 +1,10 @@
+let data = [5,7,8,9,4];
+//console.log(data.push(2));
+//console.log(data.pop());
+//console.log(data.shift());
+//console.log(data.unshift(2));
+console.log(data.splice(2,2));
+
+
+
+console.log(data);
