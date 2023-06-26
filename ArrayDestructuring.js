b98bf21c8af4a116d07b2626b54e3d1965ed2281
lilimelishvili <91181560+lilimelishvili@users.noreply.github.com>
2023-06-26 18:49:19 +0400

    Array Destructuring

---
+++ ArrayDestructuring.js
@@ -0,0 +1,20 @@
+//let nums = [5,7,2,4]
+//console.log(nums);
+
+//let [a,b,,d] = nums;
+//console.log(d);
+
+
+//let a = 6;
+//let b = 8 ;
+//[a,b] =  [b,a]
+
+//console.log(a,b);
+
+
+let wards = "My name is Lili".split(' ');
+
+let [a,b,c,d,e] = wards
+
+console.log(d);
+
